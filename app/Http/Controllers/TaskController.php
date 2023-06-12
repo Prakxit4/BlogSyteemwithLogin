9f49552e0781b4167a2a65235edc1e6285d51f1e
prakshit poudel <prakxit123@gmail.com>
2023-06-12 12:17:02 +0545

    Added new feaeture

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,7 +12,7 @@ class TaskController extends Controller
 {
     public function index()
     {
-        $tasks = Task::all();
+        $tasks = Task::latest()->get();
         return view('tasks.index', compact('tasks'));
     }
 
